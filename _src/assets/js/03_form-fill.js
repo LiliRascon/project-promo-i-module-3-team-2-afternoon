ea80d5a15cf62e61f0a36468e2fb6a523f1fe65f
AnaValca <avc92j@gmail.com>
2020-03-27 11:48:27 +0100

    delete

--- _src/assets/js/03_form-fill.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-function copyInfoName(event) {
-  previewFullName.innerHTML = fullName.value || 'Nombre Apellido';
-}
-function copyInfoJob() {
-  previewJobPosition.innerHTML = jobPosition.value || 'Puesto';
-}
