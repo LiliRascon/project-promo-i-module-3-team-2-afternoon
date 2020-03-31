d5d84438dd0d7fb80f336de2964a995f5c2055ab
maitevillar <sayhello.maite@gmail.com>
2020-03-31 11:50:44 +0200

    formgeneral details

--- src/components/FormGeneral.js
+++ src/components/FormGeneral.js
@@ -1,1 +1,16 @@
 import React from 'react';
+import Collapsables from './Form-components/Collapsables';
+import DesignForm from './Form-components/DesignForm';
+import FillForm from './Form-components/FillForm';
+import ShareFormValidation from './Form-components/ShareFormValidation';
+
+class FormGeneral extends React.Component {
+    render(){
+        return(
+            //body
+            //<Collapsables/>
+        );
+    }
+}
+
+export default FormGeneral;
